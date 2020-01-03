93f5e8e473a2d7431d420b937ff82ff916597e6c
Christos Panourgias <cpanourgias@justaddmusic.net>
2020-01-03 17:43:06 +0100

    added auth on updating profile

--- src/App.js
+++ src/App.js
@@ -89,22 +89,25 @@ class App extends Component {
   };
 
   calculateFaceLocations = (data) => {
-    return  data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box
-      const image = document.getElementById('inputimage');
-      const width = Number(image.width);
-      const height = Number(image.height);
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
-    });
+    if (data && data.outputs) {
+      return  data.outputs[0].data.regions.map(face => {
+        const clarifaiFace = face.region_info.bounding_box,
+            image = document.getElementById('inputimage'),
+            width = Number(image.width),
+            height = Number(image.height);
+        return {
+          leftCol: clarifaiFace.left_col * width,
+          topRow: clarifaiFace.top_row * height,
+          rightCol: width - (clarifaiFace.right_col * width),
+          bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+      });
+    }
+    return null;
   };
 
   displayFaceBox = (boxes) => {
-    this.setState({boxes: boxes});
+    return boxes & this.setState({boxes: boxes});
   };
 
   onInputChange = (event) => {
